be02284038db8074c16b14051a77b88b3bfa6f5a
aaron <aaron>
2025-12-27 21:23:59 -0800

    add nix development env

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Environment for homepage development";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs }: 
+    let pkgs = nixpkgs.legacyPackages."x86_64-linux";
+  in
+  {
+    devShells."x86_64-linux".default = pkgs.mkShell { 
+      packages = with pkgs; [ nodejs pnpm npx ];
+    };
+  };
+}
