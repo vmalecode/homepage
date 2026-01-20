0c2843460af27e09343282b4f3d8ba0691232f78
aaron <aaron>
2026-01-19 23:48:55 -0800

    update navbar, add dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SiteConfig from "@/config/site";
 import NavBar from "@/components/NavBar";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,11 +26,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en suppressHydrationWarning">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NavBar />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <NavBar />
+        </ThemeProvider>
         {children}
       </body>
     </html>
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,15 +1,20 @@
 import Link from "next/link"
+import { Button } from "./ui/button"
 
 const NavBar = () => {
   return (
     <nav className='h-16 bg-background/60 sticky top-0 border-b px-8
-      backdrop-blur flex justify-between'>
+      backdrop-blur items-center flex justify-between'>
       <div className="fond-bold text-xl">
         Aaron's Homepage
       </div>
-      <ul className="flex w-full justify-end">
+      <ul className="items-center flex space-x-4">
         <li><Link href={"/"}>Home</Link></li>
         <li><Link href={"/about"}>About</Link></li>
+        <li className="buttons px-4 space-x-2">
+          <Button variant="link">Login</Button>
+          <Button variant="link">Sign Up</Button>
+        </li>
       </ul>
     </nav>
   )
