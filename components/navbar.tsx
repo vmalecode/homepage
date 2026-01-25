e2c4a4d05b944fb31e9c49ff1b4d4f380f55dbb5
aaron <aaron>
2026-01-25 12:28:31 -0800

    navbar with dialog overlay

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,37 +2,43 @@ import Link from "next/link"
 import { Button } from "./ui/button"
 import { ModeToggle } from "./theme-toggle"
 import {
-  Sheet,
-  SheetContent,
-  SheetTrigger,
-} from "@/components/ui/sheet"
+  Popover,
+  PopoverContent,
+  PopoverDescription,
+  PopoverHeader,
+  PopoverTitle,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 import { Menu } from "lucide-react"
+import { ScrollArea } from "@/components/ui/scroll-area"
 import { MobileNav } from "./mobile-nav"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
 
 const NavBar = () => {
   return (
-    <nav className="sticky top-0 z-50 border-b bg-background/60 backdrop-blur">
-      <div className="flex h-14 items-center px-4 justify-between">
-
-        {/* Mobile: hamburger */}
-        <div className="lg:hidden">
-          <Sheet>
-            <SheetTrigger asChild>
-              <Button variant="ghost" size="icon">
-                <Menu className="h-5 w-5" />
-              </Button>
-            </SheetTrigger>
-
-            <SheetContent side="left" className="w-64">
-              <MobileNav />
-            </SheetContent>
-          </Sheet>
-        </div>
+    <nav className="sticky top-0  z-50 border-b bg-background/40 backdrop-blur">
+      <div className="flex h-15 items-center px-4 justify-between">
 
         {/* Desktop title */}
         <div className="hidden lg:block font-bold">
           Aaron Van Der Male
         </div>
+        <Dialog >
+          <DialogTrigger><Menu /></DialogTrigger>
+          <DialogContent className="bg-background/60 backdrop-blur">
+            <DialogTitle>Test Title</DialogTitle>
+          </DialogContent>
+        </Dialog>
 
         {/* Desktop nav */}
         <ul className="hidden md:flex gap-2">
@@ -45,8 +51,8 @@ const NavBar = () => {
         </ul>
 
         <ModeToggle />
-      </div>
-    </nav>
+      </div >
+    </nav >
   )
 }
 
